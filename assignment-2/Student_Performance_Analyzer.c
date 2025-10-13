06ff387468ee65a43fd8355a080b18d6a1575172
nishan0405 <nishanar3@gmail.com>
2025-10-13 10:12:01 +0530

    Update Student_Performance_Analyzer.c

--- assignment-2/Student_Performance_Analyzer.c
+++ assignment-2/Student_Performance_Analyzer.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "str.h"   
+#include "str.h"
 
 int main() {
     struct Student students[100];
@@ -14,9 +14,27 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        printf("\nEnter Roll No, Name, and Marks in 3 subjects: ");
-        scanf("%d %s %f %f %f", &students[i].roll, students[i].name,
-              &students[i].marks[0], &students[i].marks[1], &students[i].marks[2]);
+        printf("\n--- Enter details for Student %d ---\n", i + 1);
+
+        do {
+            printf("Enter Roll No (positive integer): ");
+            scanf("%d", &students[i].roll);
+            if (students[i].roll <= 0)
+                printf("Invalid roll number! Please enter a positive integer.\n");
+        } while (students[i].roll <= 0);
+
+        printf("Enter Name: ");
+        scanf("%s", students[i].name);
+
+        students[i].numSubjects = 3;
+        for (int j = 0; j < students[i].numSubjects; j++) {
+            do {
+                printf("Enter marks for Subject %d (0–100): ", j + 1);
+                scanf("%f", &students[i].marks[j]);
+                if (students[i].marks[j] < 0 || students[i].marks[j] > 100)
+                    printf("Invalid marks! Please enter between 0 and 100.\n");
+            } while (students[i].marks[j] < 0 || students[i].marks[j] > 100);
+        }
 
         students[i].total = calculateTotal(students[i]);
         students[i].avg = calculateAverage(students[i]);
@@ -40,7 +58,7 @@ int main() {
         printf("\n");
     }
 
-    printf("\nList of Roll Numbers : ");
+    printf("\nList of Roll Numbers: ");
     printRollNumbers(0, n);
 
     return 0;
